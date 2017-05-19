c696d9687b6c0e2199fab818b2383e80e3856712
Abigale <609262982@qq.com>
2017-05-19 10:05:34 +0800

    测试

---
+++ src/dog.js
@@ -0,0 +1,10 @@
+class Dog {
+  constructor(name) {
+    this.name = name
+  }
+  bark() {
+    return `haha ${this.name}`
+  }
+}
+
+module.exports = Dog
---
+++ src/index.js
@@ -0,0 +1,7 @@
+/* eslint-disable no-console */
+
+import Dog from './dog'
+
+const tobby = new Dog('TOBBY')
+console.log(tobby.bark())
+// console.log(navigator.serviceWorker)
